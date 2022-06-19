1f5fb9b3ade306f5380af223e5f9daed5e4bcbee
BIGSING <pushebp@163.com>
2022-06-19 12:12:54 +0800

    1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string dirInput = "E:/input";
+if (!Directory.Exists(dirInput)) {
+    Console.WriteLine("dir not exists: " + dirInput);
+    Console.ReadKey();
+    return;
+}
+
+var timeStart = DateTime.Now;
+string dirOutput = Path.Combine(dirInput, "out");
+if (!Directory.Exists(dirOutput)) { Directory.CreateDirectory(dirOutput); }
+var files = star.files.GetFiles(dirInput, ".txt");
+foreach (var filePath in files) {
+    File.WriteAllText(Path.Combine(dirInput, "out", Path.GetFileName(filePath)), star.strings.formatTextBlock(File.ReadAllText(filePath), "\r\n\r\n", "\r\n", @"\n", "\"", "\","));
+}
+
+Console.WriteLine("time used: " + (DateTime.Now - timeStart).ToString(@"hh\:mm\:ss"));
+Console.ReadKey();
